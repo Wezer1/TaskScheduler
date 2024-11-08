384ebf10b67010cdee99c01e4ad7f748df40e48e
Maksim <masyanya.maksimka@mail.ru>
2024-11-09 01:22:15 +0300

    Create BD

---
+++ src/main/java/net/proselyte/pet_project_task_scheduler/PetProjectTaskSchedulerApplication.java
@@ -0,0 +1,13 @@
+package net.proselyte.pet_project_task_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetProjectTaskSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetProjectTaskSchedulerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/proselyte/pet_project_task_scheduler/PetProjectTaskSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.proselyte.pet_project_task_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProjectTaskSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
